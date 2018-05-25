b727e6fd5f2b51178f8739b5572316b2cb5dc2f0
Kaiden Rawlinson <kaiden-rawlinson@pluralsight.com>
2018-05-24 23:38:20 -0600

    Add config for Heroku deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,9 @@ import app from './app'
 import config from './config'
 import { AddressInfo } from 'net'
 
-const server = app.listen(config.port, async () => {
+const port = process.env.PORT || config.port
+
+const server = app.listen(port, async () => {
   const { port } = <AddressInfo>server.address()
 
   console.log(`API listening at http://localhost:${port}`)
